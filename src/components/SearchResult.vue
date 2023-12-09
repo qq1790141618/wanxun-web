7b1717a21580eba912bd007203b7e9b94bf7275c
Limit Au <69846516+qq1790141618@users.noreply.github.com>
2023-12-09 10:15:32 +0800

    search box

--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -201,7 +201,7 @@ export default {
                 options.goods.push({
                     label: goods.data[i].stylenumber,
                     value: goods.data[i].stylenumber,
-                    description: i18n.goods[i18n.language],
+                    description: goods.data[i].stylenumber['store-id'],
                     otherTag: goodsbrand.value,
                     type: 'goods',
                     avatar: goods.data[i]['main-image'] == null ? 'https://cdn.fixeam.com/tw/colorful/shopping.png' : JSON.parse(goods.data[i]['main-image'])[0]
